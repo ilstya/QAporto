0ef801d110daf05007195bd2736a928ce852aeaf
ilstya <ilhamsetyamuhti2000@gmail.com>
2025-02-13 08:45:25 +0700

    update fix workflow

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,18 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+        //features = "src/test/resources/features",
+        //glue = {"api.stepdefs", "web.stepdefs"},
+        //tags = "@api or @web",
+        //plugin = {
+                //"pretty",
+                //"html:target/cucumber-reports.html",
+                //"json:target/cucumber.json"
+        //}
+//)
+
+public class TestRunner {
+}
